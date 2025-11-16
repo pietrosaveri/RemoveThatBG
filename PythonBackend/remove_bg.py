28a663001c4c747524eea9cda071b2d2b80ec75b
Pietro Saveri <pietrosaveri24@gmail.com>
2025-11-16 02:04:32 +0100

    update gitignore and LFS

---
+++ PythonBackend/remove_bg.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ded64b23e7b02f3277b0a3d6f355014d3fee172cae5d33b19243e22433edca68
+size 570
---
+++ PythonBackend/remove_bg.spec
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:016881ac17aeab22e41bdc3b29c7061ecc56cf92a74624fcc373f70805db9a36
+size 934
