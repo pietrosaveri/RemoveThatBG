4a43a19bd64ebc7c77c029f0a6167e628da1d781
Pietro Saveri <pietrosaveri24@gmail.com>
2025-11-30 18:32:49 +0100

    removed python backedn

--- PythonBackend/server.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:48596aa0a113b36123e64bfdbe3d9adcb5f36972579e646eddc8efc5c754a37f
-size 8317
--- PythonBackend/setup_venv.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:89d53f4b074bf064fd38e063bc84659ec42659d3690d28a559046ea76094b299
-size 7809
